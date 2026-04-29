8998bcb8e7795728ea3afb0700e52d67cc626b4e
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2026-04-29 17:54:29 +0100

    skill entity and model

---
+++ backend/src/SkillMatrixLlm.Api/Data/Entities/Skill.cs
@@ -0,0 +1,8 @@
+namespace SkillMatrixLlm.Api.Data.Entities;
+
+public class Skill
+{
+  public Guid Id { get; set; }
+
+  public required string Name { get; set; }
+}
---
+++ backend/src/SkillMatrixLlm.Api/Models/Skill.cs
@@ -0,0 +1,8 @@
+namespace SkillMatrixLlm.Api.Models;
+
+public class Skill
+{
+  public Guid Id { get; set; }
+
+  public required string Name { get; set; }
+}
