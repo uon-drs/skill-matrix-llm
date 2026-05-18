ff16f206092453f57b4f56fefdab0937251ead8a
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2026-05-18 11:54:25 +0100

    add LLM service to Program.cs

--- backend/src/SkillMatrixLlm.LlmWorker/Program.cs
+++ backend/src/SkillMatrixLlm.LlmWorker/Program.cs
@@ -1,6 +1,22 @@
+using Anthropic;
+using Microsoft.Extensions.Options;
 using SkillMatrixLlm.LlmWorker;
+using SkillMatrixLlm.LlmWorker.Config;
+using SkillMatrixLlm.LlmWorker.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+/// Set up LLM Analysis service
+builder.Services.AddSingleton(sp =>
+  {
+    var opts = sp.GetRequiredService<IOptions<WorkerOptions>>().Value;
+    return new AnthropicClient() { ApiKey = opts.AnthropicApiKey };
+  }
+);
+builder.Services.AddSingleton(sp => sp.GetRequiredService<AnthropicClient>().Messages);
+builder.Services.AddSingleton<ILlmAnalysisService, ClaudeAnalysisService>();
+
+/// Add worker which executes the LLM Analysis service
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
